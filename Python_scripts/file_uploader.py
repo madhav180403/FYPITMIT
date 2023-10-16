c8fa2284df21c47783120f559e45ed7a719eab16
madhav180403 <121374551+madhav180403@users.noreply.github.com>
2023-10-16 20:30:54 +0530

    Added file_score_handler.py and other changes

--- Python_scripts/file_uploader.py
+++ Python_scripts/file_uploader.py
@@ -2,25 +2,20 @@ from googleapiclient.http import MediaFileUpload
 from Google import Create_Service
 import os
 
-cnt=4482
-
 files = os.listdir("Articles/")
 
-files.sort()
-
+files.sort() #Getting a sorted list of files in the directory
 
 CLIENT_SECRET_FILE = r'Other_files\client_secret.json'
 API_NAME = 'drive'
 API_VERSION = 'v3'
-SCOPES = ['https://www.googleapis.com/auth/drive']
+SCOPES = ['https://www.googleapis.com/auth/drive'] 
 
-service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES)
+service = Create_Service(CLIENT_SECRET_FILE, API_NAME, API_VERSION, SCOPES) #Creating the service to add files to the Drive account
 
-folder_ids = ['1HFC4GGUVQatcEWLxcOYchmhGzWNukv2R', '1V4gOBla8aJf-e1BKKRGYACflbSB6bHuV','1BnNVRxjE-M-lH1hNDuI9vtj3IONwvdhL','1DafBWoPqLt7Nf3tCtzERcffLA_7CmVgS']
+folder_ids = ['1HFC4GGUVQatcEWLxcOYchmhGzWNukv2R', '1V4gOBla8aJf-e1BKKRGYACflbSB6bHuV','1BnNVRxjE-M-lH1hNDuI9vtj3IONwvdhL','1DafBWoPqLt7Nf3tCtzERcffLA_7CmVgS'] #Passing the ID of each folder of the Drive account, first one being primary, others being secondary nodes
 
-for i in range(5000, len(files)+1):
-
-    print(cnt)
+for i in range(len(files)+1):
 
     file_name = 'Articles\\' + files[i]
 
@@ -36,6 +31,4 @@ for i in range(5000, len(files)+1):
             body=file_metadata,
 
             media_body=media_content
-        ).execute()
-
-    cnt+=1
+        ).execute() #Adding each file of the directory to all the 4 nodes
--- Python_scripts/web_scraper.py
+++ Python_scripts/web_scraper.py
@@ -1,33 +1,33 @@
 from requests_html import HTMLSession
 import os
 
-files = os.listdir("Articles/")
+files = os.listdir("Articles/") #Get a list of articles present in the directory, to avoid new files overwriting old files
 
 cnt = len(files)+1
 
 for i in range(195,501):
 
-    session1 = HTMLSession()
+    session1 = HTMLSession() #Creation of the HTMLSession
 
-    url = 'https://www.espncricinfo.com/cricket-news?page='+str(i)
+    url = 'https://www.espncricinfo.com/cricket-news?page='+str(i) #The URL from which data is to be scraped
 
     page = session1.get(url)
     
-    print(page)
+    print(page) #Getting Connection status, Code 200 indicates "OK"
 
-    page.html.render(timeout=15)
+    page.html.render(timeout=15) 
 
-    article_list = page.html.find('.ds-border-b.ds-border-line.ds-p-4')
+    article_list = page.html.find('.ds-border-b.ds-border-line.ds-p-4') #Finding html elements based on this class, which includes the link of the article
 
     articles = [i.find("a") for i in article_list]
 
-    articles = [i for sublist in articles for i in sublist]
+    articles = [i for sublist in articles for i in sublist] #Flattening the list of articles
 
     for article in articles:
 
         session2 = HTMLSession()
 
-        article_link = "https://espncricinfo.com"+article.attrs['href']
+        article_link = "https://espncricinfo.com"+article.attrs['href'] #Performing web scraping for each article
 
         if('espncricinfo' not in str(article_link)):
             continue
@@ -38,9 +38,9 @@ for i in range(195,501):
 
         content_title = article_page.html.find('h1')
 
-        article_content = content_title[0].text + "\n\n"
+        article_content = content_title[0].text + "\n\n" #Getting the Title of the Article
 
-        content = article_page.html.find('.ds-text-comfortable-m.ds-my-4.ci-html-content')
+        content = article_page.html.find('.ds-text-comfortable-m.ds-my-4.ci-html-content') #Getting the content of the article, as all the 'p' tags share the same class
 
         for paragraph in content:
 
@@ -51,7 +51,7 @@ for i in range(195,501):
         try:
             with open(file_name,"w+",encoding='utf-8') as file:
 
-                file.writelines(article_content)
+                file.writelines(article_content) #Adding the content of each article to a file
         
         except:
                 print("An Unknown error occurred while writing data into the file")
@@ -60,4 +60,4 @@ for i in range(195,501):
 
         session2.close()
     
-    session1.close()
+    session1.close() #Closing both sessions
