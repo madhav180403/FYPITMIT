6c845e0d8f6b1fdcf21892953ebfafe0ee980c6b
madhav180403 <121374551+madhav180403@users.noreply.github.com>
2023-11-05 11:57:34 +0530

    Added functionality to reset and update file score

--- Python_scripts/file_score_handler.py
+++ Python_scripts/file_score_handler.py
@@ -15,6 +15,16 @@ def init_file_score():
     for i in files:
         data += [{"Name" : i , "File_Score" : 0}] #Initialising a list to store the file score of all the articles in the directory
 
+        cnt+=1
+        if(cnt==5000):
+            break
+
+
     x = collection.insert_many(data) #Storing the list in the MongoDB collection
 
-init_file_score()
+def update_file_score(file,score):
+
+    query = {"Name": file}
+    update = {"$inc": {"File_Score": score}}
+
+    collection.update_one(query, update)
